1ffebaf4d292ee09b13ea88a0d7f9aa26de372a4
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-10-10 13:17:31 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const valorantweapons = {
+    vandal: {
+        price: 2900,
+        range: "mid",
+        killcount: 5561,
+        averagedmg: "743 damage per match",
+    },
+    shorty: {
+        price: 300,
+        range: "close",
+        killcount: 86,
+        averagedmg: "36.3 damage per match",
+    },
+    spectre: {
+        price: 
+    }
+} 
