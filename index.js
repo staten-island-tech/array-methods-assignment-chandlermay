5e373bfdef90862ead20545f12069812cf9b87ad
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-10-13 13:15:38 -0400

    10/13

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const valorantweapons = {
+const valorantweapons = [
     vandal: {
         price: 2900,
         range: "mid",
         killcount: 5561,
         averagedmg: "743 damage per match",
+        skins: ["reaver" , "oni" , "prelude to chaos" , "neptune" , "prime"]
     },
     shorty: {
         price: 300,
         range: "close",
         killcount: 86,
         averagedmg: "36.3 damage per match",
+        skins: ["prelude to chaos" , "oni" , "neptune"]
     },
     spectre: {
-        price: 
-    }
-} 
+        price: 1600,
+        range: "close",
+        killcount: 430,
+        averagedmg: "800 damage per match",
+        skins: ["prime" , "reaver" , "neptune" , "forsaken"]
+    },
+    operator: {
+        price: 4700,
+        range: "long",
+        killcount: 3340,
+        averagedmg: "650 damage per match",
+        skins: ["reaver" , "forsaken" , "prelude to chaos" , "glitchpop"]
+    },
+]
